cb5173d7e21a464cc73aebbfa155b2bc5de05ae1
Ryan <admin@Ryans-MacBook-Air.local>
2016-02-27 16:45:52 +1300

    Adding experimenting NLP parsing

--- gym.rb
+++ gym.rb
@@ -3,13 +3,15 @@ require 'open-uri'
 require 'sqlite3'
 require 'slop'
 require 'terminal-table'
-
+require 'nickel'
 
 # Open a database
 $db = SQLite3::Database.new "gym.db"
-
-
-
+$gym_ids = {
+    "city" => "96382586-e31c-df11-9eaa-0050568522bb",
+    "britomart" => "744366a6-c70b-e011-87c7-0050568522bb",
+    "takapuna" => "98382586-e31c-df11-9eaa-0050568522bb",  
+    "newmarket" => "b6aa431c-ce1a-e511-a02f-0050568522bb"}
 
 def get_and_store_times()
   # Create a table
@@ -29,14 +31,9 @@ def get_and_store_times()
 
 
   base_url = "https://www.lesmills.co.nz/timetable-calander.ashx?club="
-  gym_ids = {
-    "city" => "96382586-e31c-df11-9eaa-0050568522bb",
-    "britomart" => "744366a6-c70b-e011-87c7-0050568522bb",
-    "takapuna" => "98382586-e31c-df11-9eaa-0050568522bb",
-    "newmarket" => "b6aa431c-ce1a-e511-a02f-0050568522bb"}
   insert_query = "INSERT OR IGNORE INTO timetable (gym,class,location,start_datetime,end_datetime) VALUES (?,?,?,?,?)"
   # For each gym:
-  gym_ids.each do |gym, id|
+  $gym_ids.each do |gym, id|
   # Download the the ICS File
     begin
       gym_cal = open(base_url+id)
@@ -60,8 +57,9 @@ begin
     o.string '-g', '--gym', 'The gym you want classes from e.g. britomart, newmarket, city etc. (default any)', default: ""
     o.string '-a', '--after', 'The time that you want classes after e.g. 13:30 (default now)', default: DateTime.now.strftime("%H:%M:%S")
     o.string '-b', '--before', 'The time that you want classes before e.g. 17:30 (default 23:59:59)', default: "23:59:59"
-    o.string '-d', '--day', 'The day you want classes for (default today)', default: DateTime.now.strftime("%Y-%m-%d")
+    o.string '-d', '--day', 'The day you want classes for (default today) e.g. "today", "next tuesday"', default: DateTime.now.strftime("%Y-%m-%d")
     o.string '-c', '--class', 'The class that you want times for e.g. Grit (default any)', default: ""
+    o.string '-s', '--search', 'Experimental NLP parsing of dates e.g. tomorrow after 6pm'
     o.bool '-nf', '--nofetch', 'Will not fetch new timetable info before searching', default: false
     o.on '--help' do
       puts o
@@ -79,13 +77,39 @@ begin
     if !opts.nofetch?
         get_and_store_times()
     end
+    
+    if opts.day?
+        day = Nickel.parse(opts[:day])
+        if day.occurrences.empty?
+            day_string = DateTime.now("%Y-%m-%d")
+        else
+            day_string = day.occurrences.first.start_date.to_date.to_s
+        end
+    end
+
+   if opts.search?
+        search_string = Nickel.parse(opts[:search])
+        start_string = search_string.occurrences.first.start_time.to_time.strftime('%H:%M:%S')
+        end_string = search_string.occurrences.first.end_time.to_time.strftime('%H:%M:%S')
+        day_string = search_string.occurrences.first.start_date.to_date.strftime('%Y-%m-%d')
+        gym_string = $gym_ids.keys.map {|x| search_string.message.include?(x) ? x : nil}.compact.first 
+   else
+       start_string = opts[:after]
+       end_string = opts[:before]
+       class_string = opts[:class]
+       gym_string = opts[:gym]
+   end
+
+    
     query_string = "SELECT gym, class, location, TIME(start_datetime) from timetable
-    WHERE gym like '%#{opts[:gym]}%'
-    AND TIME(start_datetime) > \"#{opts[:after]}\"
-    AND TIME(end_datetime) <  \"#{opts[:before]}\"
-    AND DATE(start_datetime) = \"#{opts[:day]}\"
-    AND class like '%#{opts[:class]}%'
+    WHERE gym like '%#{gym_string}%'
+    AND TIME(start_datetime) > \"#{start_string}\"
+    AND TIME(end_datetime) <  \"#{end_string}\"
+    AND DATE(start_datetime) = \"#{day_string}\"
+    AND class like '%#{class_string}%'
     order by start_datetime asc"
+
+    # puts query_string
 
     db_rows = $db.execute(query_string)
     table_rows = []
